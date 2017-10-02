a1a93dc34d903e6372701e7b9f87da80a9bddfb7
Fernando Espíndola <fernando.espindola@hp.com>
2017-10-02 08:39:30 -0300

    WIP

--- tests/test_storiesByCharacterIterable.py
+++ tests/test_storiesByCharacterIterable.py
@@ -1,9 +1,13 @@
 from unittest import TestCase
+from unittest.mock import patch
+
+import fakeredis
 
 from marvel.iterables import StoriesByCharacterIterable
 
 
 class TestStoriesByCharacterIterable(TestCase):
+    @patch('marvel.request.r', fakeredis.FakeStrictRedis())
     def test_next(self):
         count = 0
         for c in StoriesByCharacterIterable(1009351):
